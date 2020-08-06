5226caff8141415b28ff6e4db98ce7c854d6618f
jose l <jose1989luiz@gmail.com>
2020-08-06 13:35:17 -0500

    first commit

---
+++ src/app/Practica/auto.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-auto',
+    templateUrl: './auto.component.html',
+    styleUrls: ['./auto.component.scss']
+})
+export class AutoComponent{ }
